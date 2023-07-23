64e9a07d2f6e12497dc6aae092218d23238845c6
dzc0d3r <m4dcod3r@protonmail.com>
2023-07-23 02:03:09 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+
+}
+
+
+function mondayWork(duty = "go to the office") {
+    return `This Monday, I will ${duty}.`
+
+}
+
+
+function wrapAdjective(symbol) {
+    const result = function (adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`
+
+    }
+    return result
+
+}
